5a42aa2c19ee651477160dd87c7b87f684db5d9b
Ridhwan Hussain <ridhwanplayzzz@gmail.com>
2025-04-11 23:23:22 +0100

    Configure Better Auth!

---
+++ lib/auth.ts
@@ -0,0 +1,16 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient()
+
+export const auth = betterAuth({
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: true,
+        minPasswordLength: 10,
+    },
+    database: prismaAdapter(prisma, {
+        provider: "postgresql",
+    }),
+})
